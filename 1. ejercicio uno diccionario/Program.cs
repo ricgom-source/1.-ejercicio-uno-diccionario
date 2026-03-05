1980a61fef4a4482e3554b1292361d2e08942b1a
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-05 10:58:48 -0600

    agregado mostrar info

--- 1. ejercicio uno diccionario/Program.cs
+++ 1. ejercicio uno diccionario/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,15 +27,42 @@ namespace _1.ejercicio_uno_diccionario
 
             Console.WriteLine();
 
-            Console.WriteLine("Ingrese el id del estudiante que usted quiera para ver su nombre:");
+            Console.Write("Ingrese el id del estudiante que usted quiera para ver su nombre:");
 
             int ing = int.Parse(Console.ReadLine());
 
+            foreach (var item in idestudiantes)
+
+            {
+
+                if (ing < 0 || ing > 4)
+
+                {
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Usted no ha ingresado un dato valido");
+
+                    Console.ReadKey();
+
+                    Console.Clear();
+
+                    break;
+
+                }
+
+                else if (ing == 1)
+
+                {
+
+                    Console.WriteLine();
+
+                    Console.WriteLine($"El estudiante con numero de carnet {1}, su nombre es:{item.Value} ");
+
+                }
 
 
-
-
-
+            }
 
 
 
