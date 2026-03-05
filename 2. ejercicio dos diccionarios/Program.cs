0826f877da2c70d69aed0820b754b28739571e7a
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-05 11:18:55 -0600

    agregado el progra dos

---
+++ 2. ejercicio dos diccionarios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.ejercicio_dos_diccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Dictionary<int,string> productos = new Dictionary<int,string>();
+
+            productos.Add(123456, "Coco");
+
+            productos.Add(654321, "Manzana");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
