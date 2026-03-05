c17ea438632e29f969fa5af04428f5743439bd7f
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-05 10:44:57 -0600

    Actualizacion del ejercicio uno

--- 1. ejercicio uno diccionario/Program.cs
+++ 1. ejercicio uno diccionario/Program.cs
@@ -11,9 +11,23 @@ namespace _1.ejercicio_uno_diccionario
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Hola mundo");
+            Dictionary<int,string> idestudiantes = new Dictionary<int,string>();
 
+            idestudiantes.Add(1, "Ricardo");
 
+            idestudiantes.Add(2, "Juan");
+
+            idestudiantes.Add(3, "Carlos");
+
+            idestudiantes.Add(4, "Otto");
+
+            Console.WriteLine("Usted ha ingresado a la minibase de datos de estudiantes");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el id del estudiante que usted quiera para ver su nombre:");
+
+            int ing = int.Parse(Console.ReadLine());
 
 
 
