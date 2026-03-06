cc910c80e646a81dc74e2eedab3ebc6cb119611c
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-06 08:39:17 -0600

    Experimento con diccionario ejercicio 3

--- 3. ejercicio tres diccionarios/Program.cs
+++ 3. ejercicio tres diccionarios/Program.cs
@@ -9,7 +9,42 @@ namespace _3.ejercicio_tres_diccionarios
     internal class Program
     {
         static void Main(string[] args)
+
         {
+
+            Dictionary<string,double> nombreynota = new Dictionary<string,double>();
+
+            nombreynota.Add("Juan", 88.56);
+
+            nombreynota.Add("Roberto", 78.70);
+
+            nombreynota.Add("Lucas", 67.85);
+
+            nombreynota.Add("Carlos", 90.84);
+
+            string nomb;
+
+            Console.Write("Ingrese el nombre del estudiante del cual quiere ver la nota:_ ");
+
+            nomb = Console.ReadLine();
+
+            Console.WriteLine();
+
+            if(nomb=="Juan")
+
+            {
+
+                Console.WriteLine($"La nota de 'Juan' es:_{nombreynota["Juan"]}");
+
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
