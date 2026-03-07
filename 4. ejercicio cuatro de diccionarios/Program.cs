689a5825f839554e6482e0ce14b5d6a9f7f08c60
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-07 14:43:43 -0600

    Agregado de progarma 4 diccionario

---
+++ 4. ejercicio cuatro de diccionarios/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.ejercicio_cuatro_de_diccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            Dictionary<int, string> ciudades = new Dictionary<int, string>();
+
+            ciudades.Add(09001, "Quetzaltenango");
+
+            ciudades.Add(01057, "Mixco");
+
+            ciudades.Add(16001, "Cobán");
+
+            ciudades.Add(12001, "San Marcos");
+
+            ciudades.Add(03001, "Antigua Guatemala");
+
+            Console.WriteLine();
+
+
+
+
+
+
+        }
+    }
+}
