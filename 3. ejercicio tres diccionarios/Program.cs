da26df44afacc080c74798069b0bca5494dd4d66
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-07 08:53:18 -0600

    Agregado final de ejercicio 3 completo

--- 3. ejercicio tres diccionarios/Program.cs
+++ 3. ejercicio tres diccionarios/Program.cs
@@ -24,39 +24,75 @@ namespace _3.ejercicio_tres_diccionarios
 
             string nomb;
 
-            Console.Write("Ingrese el nombre del estudiante del cual quiere ver la nota:_ ");
-
-            nomb = Console.ReadLine();
-
-            Console.WriteLine();
-
-            if (nomb=="Juan")
-
+            do
             {
 
-                Console.WriteLine($"La nota de 'Juan' es de:_ {nombreynota["Juan"]} puntos");
+
+
+                Console.Write("Ingrese el nombre del estudiante del cual quiere ver la nota:_ ");
+
+                nomb = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (nomb == "Juan")
+
+                {
+
+                    Console.WriteLine($"La nota de 'Juan' es de:_ {nombreynota["Juan"]} puntos");
+
+                    break;
+
+                }
+
+                else if (nomb == "Roberto")
+
+                {
+
+                    Console.WriteLine($"La nota de 'Roberto' es de:_ {nombreynota["Roberto"]} puntos");
+
+                    break;
+
+                }
+
+                else if (nomb == "Lucas")
+
+                {
+
+                    Console.WriteLine($"La nota de 'Lucas' es de:_ {nombreynota["Lucas"]} puntos");
+
+                    break;
+
+                }
+
+                else if (nomb == "Carlos")
+
+                {
+                    Console.WriteLine($"La nota de 'Carlos' es de:_ {nombreynota["Carlos"]} puntos");
+
+                    break;
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("Usted no ha ingresado un nombre válido");
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Presione enter, e intentelo de nuevo");
+
+                    Console.ReadKey();
+
+                    Console.Clear();
+
+                }
 
             }
 
-            else if (nomb == "Roberto")
-
-            {
-
-                Console.WriteLine($"La nota de 'Roberto' es de:_ {nombreynota["Roberto"]} puntos");
-
-            }
-
-            else if (nomb == "Lucas")
-
-            {
-
-                Console.WriteLine($"La nota de 'Lucas' es de:_ {nombreynota["Lucas"]} puntos");
-
-            }
-
-
-
-
+            while (nomb != "");
 
         }
     }
