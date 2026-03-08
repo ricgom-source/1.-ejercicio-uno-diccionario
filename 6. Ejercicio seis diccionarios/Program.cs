637e5d50f6016250dab11b15b1d694ce09fad118
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-07 20:24:41 -0600

    Agregado ejercicio seis

---
+++ 6. Ejercicio seis diccionarios/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Ejercicio_seis_diccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Dictionary<int,string> alumnos = new Dictionary<int,string>();
+
+            alumnos.Add(1221, "Lucas");
+
+            alumnos.Add(4554, "Mario");
+
+            alumnos.Add(7887, "Jose");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
