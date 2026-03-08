28a1f587729d05fdeda2fb3fccb02627a71300e6
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-07 20:42:29 -0600

    Agregado final final ejercicio seis

--- 6. Ejercicio seis diccionarios/Program.cs
+++ 6. Ejercicio seis diccionarios/Program.cs
@@ -14,20 +14,56 @@ namespace _6.Ejercicio_seis_diccionarios
         {
             Dictionary<int,string> alumnos = new Dictionary<int,string>();
 
+            int carnet;
+
             alumnos.Add(1221, "Lucas");
 
             alumnos.Add(4554, "Mario");
 
             alumnos.Add(7887, "Jose");
 
+            Console.WriteLine("Los alumnos registrados son los siguientes:_ ");
+
+            Console.WriteLine();
+
+            foreach (var item in alumnos)
+
+            {
+                Console.WriteLine($"alumno con No. de carnet: {item.Key} , su nombre es: {item.Value}");
+
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Ingrese el número de carnet del estudiante que quiera eliminar:_ ");
+
+            carnet = int.Parse(Console.ReadLine());
+
+            alumnos.Remove(carnet);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"usted ha eliminado al alumno con No. de carnet {carnet}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Los alumnos restantes son:");
+
+            Console.WriteLine();
+
+            foreach(var newitem in alumnos)
+
+            {
+                Console.WriteLine($"alumno con No. de carnet: {newitem.Key}, su nombre es: {newitem.Value}");
+
+                Console.WriteLine();
 
 
+            }
 
-
-
-
-
-
+           
 
 
 
