19c5a655aa9a9fc66d36c6d631a7e2c64cec7dde
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-07 20:52:09 -0600

    Agregado de ejercicio siete

---
+++ 7. Ejercicio siete diccionarios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Ejercicio_siete_diccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
